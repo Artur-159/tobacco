ff8ccd1454ed824402b307b945428095488b1fc0
Artur-159 <arturzadelyan99@gmail.com>
2025-04-26 07:37:18 -0700

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  stats: {
+    loggingDebug: ["sass-loader"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              api: "modern-compiler",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
